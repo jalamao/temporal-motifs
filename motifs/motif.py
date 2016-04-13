9fc2f9ed1c0618a8066a5d9ebf473525cbe6dde6
Andy Mellor <mellor91@hotmail.co.uk>
2016-04-13 12:09:51 +0100

    Added simple documentation

--- motifs/motif.py
+++ motifs/motif.py
@@ -1,46 +1,56 @@
 # Tools for handling a motif - creation, aggregation, plotting.
 from collections import namedtuple
 
-
-lettermap = dict(enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-inv_lettermap = {val: key for key, val in lettermap.items()}
-
 class Motif(object):
+    """
+    A class for temporal motifs.
+
+    Attributes:
+        lettermap (dict): A mapping of numbers to letters.
+        inv_lettermap (dict): A mapping of letters to numbers.
+    """
 
     lettermap = dict(enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     inv_lettermap = {val: key for key, val in lettermap.items()}
     
-    def __init__(self, iterable, undirected=False,
-                                 duration=False,
-                                 edge_color=False,
-                                 source_color=False,
-                                 target_color=False,
-                                 node=None):
+    def __init__(self, iterable, undirected=False):
         """
-        Class: Motif
-        
+        Initialises the motif class.
+
+        Args:
+            iterable (iterable): Contains a series of events (source, target, time, ...)
+            undirected (bool): States whether the motif is undirected (default:False)
         """
 
         self._text = None
-        self.node = node
-        self._duration = duration
-        self._edge_color = edge_color
-        self._source_color = source_color
-        self._target_color = target_color
+        self._duration = False
+        self._edge_color = False
+        self._source_color = False
+        self._target_color = False
         self._undirected = undirected
 
         if isinstance(iterable, basestring):
             iterable = string_to_iterable(iterable)
 
         iterable = self._clean_input(iterable)
-        self._process_input(iterable)
+        self._extract_motif_information(iterable)
         
 
 
     def _clean_input(self, iterable):
         """
         Cleans input so that it is of the form (source, target, time, duration, type)
+        
+        Note:
+            1. iterable does not need to be ordered.
+
+        Args:
+            iterable (iterable): An series of tuples (source, target, time, ...)
+
+        Returns:
+            iterable (list): A list of namedtuples.
         """
+
         iterable = list(iterable)
         event_format = infer_event_type(iterable[0])
         for attribute in event_format[3:]:
@@ -50,10 +60,18 @@ class Motif(object):
         iterable.sort(key = lambda x: x.time, reverse=False) # Sort by timestamp
         return iterable
     
-    def _process_input(self, iterable):
+    def _extract_motif_information(self, iterable):
         """
-        Processes an iterable into a motif.
-        Extracts edge durations, edge colors, interevent times and the motif itself.
+        Extracts all information from the iterable to create the motif.
+
+        Note:
+            Extracts edge durations, edge colors, interevent times and the motif itself.
+        
+        Args:
+            iterable (iterable):
+
+        Returns:
+            None
         """
         
         self.nodemap = {}
@@ -87,24 +105,33 @@ class Motif(object):
         
     @property
     def text(self):
-        """ Returns the standardised text representation of the motif. """
+        """str: Returns the standardised text representation of the motif. """
         if self._text is not None: return self._text
         self._text = self._create_motif_text()
         return self._text
 
     @property
     def type(self):
-        """ Returns the type of motif. """
-
+        """str: Returns the type of motif (directed/colored). """
         attributes = ['_duration', '_edge_color', '_source_color', '_target_color', '_undirected']
         string = '\n'.join(['{} : {}'.format(att[1:], getattr(self, att)) for att in attributes])
         return string
 
     def _create_motif_text(self):
         """ 
-        Returns the standardised text representation of the motif.
-        If edges are colored motifs returned are of the form [ABxBCy].
-        If edges are undirected then each edge is sorted alphabetically.
+        Constructs the standardised text representation of the motif.
+
+        Note:
+            If edges and nodes are plain motifs are of the form (AB-BA).
+            If edges are colored motifs returned are of the form (ABx-BCy).
+            If edges are undirected then each edge is sorted alphabetically.
+            If nodes are colored then motifs are of the form (A1B2-B2A1).
+
+        Args:
+            None
+
+        Returns:
+            str: the standardised motif text representation.
         """
         
         if self._edge_color:
@@ -126,7 +153,7 @@ class Motif(object):
         return '-'.join(s)
         
     def __repr__(self):
-        """ Motif string representation. """
+        """ Motif representation. """
         return "<Motif {}>".format(self.text)
     
     def __str__(self):
@@ -146,7 +173,7 @@ class Motif(object):
         return self.motif[ix]
     
     def __len__(self):
-        """ Returns the length of the motif. """
+        """ Returns the length of the motif (number of events). """
         return len(self.motif)
     
     def __contains__(self, item):
@@ -162,7 +189,21 @@ class Motif(object):
         """
         return self.motif, dict(zip(self.motif, range(1,len(self)+1)))
 
+
 def infer_event_type(event):
+    """
+    Infers the properties of a tuple in the context of events.
+
+    Note:
+        Currently not robust.
+        Assumes tuple is at least length 3 and of the form (source, target, time, ...).
+
+    Args: 
+        event (tuple):
+
+    Returns:
+        event_format (list): descriptions for each item in the tuple
+    """
     
     event_format = ['source', 'target', 'time']
     if len(event)==3:
@@ -188,11 +229,25 @@ def infer_event_type(event):
         raise InvalidEventTypeError("Events need to be of the form (source, target, time, duration, edge_color, source_color, target_color)")
 
 class InvalidEventTypeError(BaseException):
-    """ """
+    """ Raised when an invalid event is passed into a motif. """
     pass
 
 
 def string_to_iterable(string):
+    """
+    Converts a standardised motif string into a minimal iterable.
+
+    Note:
+
+    Args:
+        string (str): A standardised motif string (e.g. AB-BA)
+
+    Returns:
+        iterable (list): A list of events which form the string motif.
+
+    """
+
+    inv_lettermap = dict(zip('ABCDEFGHIJKLMNOPQRSTUVWXYZ', range(26)))
     events = string.split('-')
 
     if len(events[0]) == 2:
