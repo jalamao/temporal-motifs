e65ee5bf3fcc608b680fe6635ed9a52d138e9a43
Andy Mellor <mellor91@hotmail.co.uk>
2016-04-14 10:14:36 +0100

    Changed test to include more random edges

--- motifs/tests/test_event_graph.py
+++ motifs/tests/test_event_graph.py
@@ -10,7 +10,7 @@ class TestEventBuilding(TestCase):
 	def setUp(self):
 		self.sample_data = []
 		nodes = np.arange(20)
-		for i in range(10):
+		for i in range(100):
 			s1, s2 = np.random.choice(nodes, size=2, replace=False)
 			self.sample_data.append((s1,s2,i,0,'r'))
 		self.sample_data = pd.DataFrame(self.sample_data, columns=['source', 'target', 'time', 'duration', 'type'])
