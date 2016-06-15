750ce2fa1932dbb1ca012a4de35e3fcd72534992
Andy Mellor <mellor91@hotmail.co.uk>
2016-06-15 20:41:33 +0100

    code tidy (pycharm)

--- motifs/dynamic_decomposition.py
+++ motifs/dynamic_decomposition.py
@@ -1,67 +1,71 @@
 # Dynamical decomposition of a network.
 
 from collections import defaultdict
+
 import networkx as nx
 import pandas as pd
+import numpy as np
+
 
 # 1. Split motifs into fast and slow (ideally we want any arbitrary partition of the IETs).
 # Tail chosen as the last 50% of value/probability.
 
-#We'll just look at ALL IETs for now
+# We'll just look at ALL IETs for now
 def find_tail_value(motifs):
-	iets = []
-	for motif in motifs:
-		iets.append(motif.interevent)
-	iets = np.array(iets)
-	median = np.median(iets)
-	return median
+    iets = []
+    for motif in motifs:
+        iets.append(motif.interevent)
+    iets = np.array(iets)
+    median = np.median(iets)
+    return median
+
 
 # 2. Collect all motifs of particular types (slow ABBA e.t.c).
 
 def partition_motifs(motifs, split):
+    collect = defaultdict(list)
 
-	collect = defaultdict(list)
-	
-	def spliter(motif):
-		if motif.interevent < split:
-			return "fast"
-		else:
-			return "slow"
+    def spliter(motif):
+        if motif.interevent < split:
+            return "fast"
+        else:
+            return "slow"
 
-	for motif in motifs:
-		collect[motif.text + " ({})".format(splitter(motif))].append(motif)
+    for motif in motifs:
+        collect[motif.text + " ({})".format(splitter(motif))].append(motif)
 
-	return collect
+    return collect
+
 
 def partition_network(motifs):
-	""" Partitions motifs into events.
+    """ Partitions motifs into events.
 
-	Note:
-		Extra to "partition_motifs" and will be combined later. 
+    Note:
+        Extra to "partition_motifs" and will be combined later.
 
-	Returns:
-		Temporal networks partitioned by motif.
-	"""
-	collect = defaultdict(list)
+    Returns:
+        Temporal networks partitioned by motif.
+    """
+    collect = defaultdict(list)
 
-	for motif in motifs:
-		collect[motif.text].extend(motif.original)
-	for key, item in collect.items():
-		df = pd.DataFrame.from_records(item)
-		df = df.sort_values(by=2).reset_index(drop=True)
-		collect[key] = df
-	return collect
+    for motif in motifs:
+        collect[motif.text].extend(motif.original)
+    for key, item in collect.items():
+        df = pd.DataFrame.from_records(item)
+        df = df.sort_values(by=2).reset_index(drop=True)
+        collect[key] = df
+    return collect
+
 
 # 3. For each partition, add the edges of each event to the layer.
 
 def build_networks(partition):
-
-	layers = {}
-	for key, motifs in partition.items():
-		G = nx.DiGraph(name=key)
-		for motif in motifs:
-			for event in motif.original:
-				G.add_edge(event.source, event.target)
-		layers[key] = G
+    layers = {}
+    for key, motifs in partition.items():
+        G = nx.DiGraph(name=key)
+        for motif in motifs:
+            for event in motif.original:
+                G.add_edge(event.source, event.target)
+        layers[key] = G
 
 # 4. Build the multilayer network from each motif layer.
--- motifs/tests/test_event_graph.py
+++ motifs/tests/test_event_graph.py
@@ -1,29 +1,27 @@
-from unittest import TestCase
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+from unittest import TestCase
 
 from motifs import EventGraph
 
+
 class TestEventBuilding(TestCase):
+    def setUp(self):
+        self.sample_data = []
+        nodes = np.arange(20)
+        for i in range(100):
+            s1, s2 = np.random.choice(nodes, size=2, replace=False)
+            self.sample_data.append((s1, s2, i, 0, 'r'))
+        self.sample_data = pd.DataFrame(self.sample_data, columns=['source', 'target', 'time', 'duration', 'type'])
 
-	def setUp(self):
-		self.sample_data = []
-		nodes = np.arange(20)
-		for i in range(100):
-			s1, s2 = np.random.choice(nodes, size=2, replace=False)
-			self.sample_data.append((s1,s2,i,0,'r'))
-		self.sample_data = pd.DataFrame(self.sample_data, columns=['source', 'target', 'time', 'duration', 'type'])
+    def tearDown(self):
+        self.sample_data = None
 
-	def tearDown(self):
-		self.sample_data = None
+    def test_build(self):
+        event_graph = EventGraph(self.sample_data)
+        event_graph.build()
+        self.assertTrue(max(event_graph.in_degree().values()) <= 2)
+        self.assertTrue(max(event_graph.out_degree().values()) <= 2)
 
-	def test_build(self):
-		event_graph = EventGraph(self.sample_data)
-		event_graph.build()
-		self.assertTrue( max(event_graph.in_degree().values()) <= 2 )
-		self.assertTrue( max(event_graph.out_degree().values()) <= 2 )
-
-	def test_are_connected(self):
-		pass
-
+    def test_are_connected(self):
+        pass
