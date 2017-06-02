92cc24e4fb8c2d195760891f3126b5cddf481a41
Andy Mellor <mellor91@hotmail.co.uk>
2017-06-02 12:35:12 +0100

    Added tree drawing capability

--- motifs/drawing.py
+++ motifs/drawing.py
@@ -2,9 +2,91 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
+from .auxiliary import *
+
 """
 These functions are copied over from legacy code so are not pretty, but do the job for now.
 """
+
+def draw_tree(tree, dts, min_size=None, *args, **kwargs):
+    """
+    Draws a heirarchical tree
+
+    Kwargs:
+        font_size:
+        node_scale:
+        figsize:
+    """
+
+    def populate_nx_tree(G, tree):
+        for element in tree:
+            if isinstance(element, Iterable) and not isinstance(element, (int)):
+                G.add_node(element, attr_dict={'size':len(list(flatten(element)))})
+                for minor_element in element:
+                    G.add_edge(element, minor_element)
+                populate_nx_tree(G, element)
+            else:
+                G.add_node(element, attr_dict={'size':1})
+
+    G = nx.DiGraph()           
+    populate_nx_tree(G, tree)
+    G = nx.convert_node_labels_to_integers(G)
+
+    if min_size is not None:
+        remove = [node for node, size in nx.get_node_attributes(G, 'size').items() if size < min_size]
+        G.remove_nodes_from(remove)
+
+    # Defaults
+    node_scale = kwargs.get('node_scale', 10)
+    font_size = kwargs.get('font_size', 6)
+    figsize = kwargs.get('figsize', (30,10))
+        
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    ax.set_axis_off()
+    
+    root = [ix for ix, val in nx.in_degree_centrality(G).items() if val == 0.0][0]
+    remove = [ix for ix, length in nx.shortest_path_length(G,root).items() if length > len(dts)]
+    G.remove_nodes_from(remove)
+
+    dts = sorted(dts)
+    dts.append(r'\infty')
+    layers = len(dts)
+    buffer = 0.2
+    spacer = (1 - buffer)/layers
+    for ix, dt in enumerate(dts):
+        ax.text(0, buffer + ix*spacer, r"$\Delta t = {}$".format(dt), transform=ax.transAxes,
+               fontdict={'size':2*font_size})
+    
+    pos = nx.nx_pydot.graphviz_layout(G, prog='dot')
+    sizes = nx.get_node_attributes(G, 'size')
+
+    nx.draw_networkx_labels(G, pos, labels=sizes, font_size=font_size, ax=ax)
+    nx.draw_networkx_edges(G, pos, arrows=False)
+    nx.draw_networkx_nodes(G, pos, nodelist=sizes.keys(), node_size=[x*node_scale for x in sizes.values()]);
+    
+    return None
+
+def plot_full_barcode_efficiently(teg, dt, top, ax):
+    """ Prints a barcode. """
+
+    import matplotlib
+    
+    filtered = teg.filter_edges(dt)
+    segs = []
+    tmin, tmax = 1e99, 0
+    for ix,component in enumerate(sorted(nx.weakly_connected_components(filtered), key=len, reverse=True)[:top]):
+        component = [teg.event_list.ix[i] for i in component]
+        for event in component:
+            segs.append(((event.time, ix),(event.time, ix+1)))
+            tmax = max(tmax, event.time)
+            tmin = min(tmin, event.time)
+            
+    ln_coll = matplotlib.collections.LineCollection(segs, linewidths=1, colors='k')
+    bc = ax.add_collection(ln_coll)
+    ax.set_ylim((0, top+1))
+    ax.set_xlim((tmin,tmax))   
+    return bc
 
 def draw_motif(motif, ax=None, edge_color_map=None, node_color_map=None):
     """
